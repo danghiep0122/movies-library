f84f18cb46c23e4a2005331ea7499142b6f7a0cc
Hiep <danghiep0122@gmail.com>
2023-02-21 13:19:14 +0700

    done setup 17:36

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+            <head />
+            <body>
+                <nav>
+                    <h1>logo</h1>
+                    <ul>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Sign Up</a>
+                        </li>
+                    </ul>
+                </nav>
+                {children}
+            </body>
+        </html>
+    );
+}
